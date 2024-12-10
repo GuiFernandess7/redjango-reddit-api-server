12337e715174deb7b925662dc47cd26275f2120d
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-12-10 00:34:46 -0300

    config: os getenv set to DEBUG

--- redjango/config/settings/base.py
+++ redjango/config/settings/base.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_KEY", "default-secret-key")
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
