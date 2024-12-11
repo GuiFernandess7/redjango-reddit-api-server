914114ba89da2779d977c831b6362e33124fd737
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-12-11 19:40:20 -0300

    Database configuration updated

--- redjango/config/settings/base.py
+++ redjango/config/settings/base.py
@@ -64,14 +64,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", "mydatabase"),
-        'USER': os.getenv("DB_USER", "myuser"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "mypassword"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
-    }
-}
--- redjango/config/settings/dev.py
+++ redjango/config/settings/dev.py
@@ -3,11 +3,13 @@ from config.settings.base import *
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv("DB_NAME_DEV"),
-    'USER': os.getenv("DB_USER_DEV"),
-    'PASSWORD': os.getenv("DB_PASSWORD_DEV"),
-    'HOST': os.getenv("DB_HOST_DEV"),
-    'PORT': os.getenv("DB_PORT"),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME_DEV"),
+        'USER': os.getenv("DB_USER_DEV"),
+        'PASSWORD': os.getenv("DB_PASSWORD_DEV"),
+        'HOST': os.getenv("DB_HOST_DEV"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
