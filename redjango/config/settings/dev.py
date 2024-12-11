532e6e1b8d8ea135bef6f6ab9ea0a3d42f96a878
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-12-11 19:46:45 -0300

    debug print

--- redjango/config/settings/dev.py
+++ redjango/config/settings/dev.py
@@ -3,6 +3,12 @@ from config.settings.base import *
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
+print(f"DB_NAME_DEV: {os.getenv('DB_NAME_DEV')}")
+print(f"DB_USER_DEV: {os.getenv('DB_USER_DEV')}")
+print(f"DB_PASSWORD_DEV: {os.getenv('DB_PASSWORD_DEV')}")
+print(f"DB_HOST_DEV: {os.getenv('DB_HOST_DEV')}")
+print(f"DB_PORT: {os.getenv('DB_PORT')}")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
